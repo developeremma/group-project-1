6780416206f342946e4b79d0a614f587f7acb836
Emma Morales <emorales@developeremma.com>
2018-01-03 23:06:43 -0800

    adding sample weather api
    testing

---
+++ assets/weather.js
@@ -0,0 +1,12 @@
+$(document).ready(function($) {
+  $.ajax({
+  url : "http://api.wunderground.com/api/4356bb932290f7bc/geolookup/q/autoip.json",
+  dataType : "jsonp",
+  success : function(parsed_json) {
+  var location = parsed_json['location']['city'];
+  var temp_f = parsed_json['current_observation']['temp_f'];
+  alert("Current temperature in " + location + " is: " + temp_f);
+  }
+  });
+});
+$("#sosBanner").append(temp_f);
