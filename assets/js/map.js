b9b34493dbe3c94936323513268626e4e29825ec
shivraxy <31151144+shivraxy@users.noreply.github.com>
2018-01-08 21:38:43 -0800

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -195,7 +195,7 @@ $('#hospitalsButton').on('click', function(event) {
         obj = snapshot.val();
         for (var key in obj) {
             if (obj.hasOwnProperty(key)) {
-                $(".btn-group-vertical").append('<button type="list-button" class="btn btn-danger sosbuttons" place_type=' + obj[key].type + ' place_id="' + obj[key].name.concat(obj[key].place_id) + '" id="listbuttons">' + obj[key].name + '<p> Reported Open ? : ' + obj[key].reported + '</p> </button> ');
+                $(".btn-group-vertical").append('<button type="list-button" style = "white-space: normal;" class="btn btn-danger sosbuttons" place_type=' + obj[key].type + ' place_id="' + obj[key].name.concat(obj[key].place_id) + '" id="listbuttons">' + obj[key].name + '<p> Reported Open ? : ' + obj[key].reported + '</p> </button> ');
 //                 $("#map").height(500); 
             }
             var MarkerLatLng = { lat: obj[key].latitude, lng: obj[key].longitude };
@@ -254,7 +254,7 @@ $('#foodButton').on('click', function(event) {
         obj = snapshot.val();
         for (var key in obj) {
             if (obj.hasOwnProperty(key)) {
-                $(".btn-group-vertical").append('<button type="list-button" class="btn btn-danger sosbuttons" place_type=' + obj[key].type + ' place_id="' + obj[key].name.concat(obj[key].place_id) + '" id="listbuttons">' + obj[key].name + '<p> Reported Open ? : ' + obj[key].reported + '</p> </button> ');
+                $(".btn-group-vertical").append('<button type="list-button" style = "white-space: normal;" class="btn btn-danger sosbuttons" place_type=' + obj[key].type + ' place_id="' + obj[key].name.concat(obj[key].place_id) + '" id="listbuttons">' + obj[key].name + '<p> Reported Open ? : ' + obj[key].reported + '</p> </button> ');
 //                 $("#map").height(500);
             }
             var MarkerLatLng = { lat: obj[key].latitude, lng: obj[key].longitude };
@@ -282,7 +282,7 @@ $('#gasButton').on('click', function(event) {
         obj = snapshot.val();
         for (var key in obj) {
             if (obj.hasOwnProperty(key)) {
-                $(".btn-group-vertical").append('<button type="list-button" class="btn btn-danger sosbuttons" place_type=' + obj[key].type + ' place_id="' + obj[key].name.concat(obj[key].place_id) + '" id="listbuttons">' + obj[key].name + '<p> Reported Open ? : ' + obj[key].reported + '</p> </button> ');
+                $(".btn-group-vertical").append('<button type="list-button" style = "white-space: normal;" class="btn btn-danger sosbuttons" place_type=' + obj[key].type + ' place_id="' + obj[key].name.concat(obj[key].place_id) + '" id="listbuttons">' + obj[key].name + '<p> Reported Open ? : ' + obj[key].reported + '</p> </button> ');
 //                 $("#map").height(500);
             }
             var MarkerLatLng = { lat: obj[key].latitude, lng: obj[key].longitude };
